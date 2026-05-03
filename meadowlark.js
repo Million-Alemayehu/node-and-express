ec8f86eabfbb3930552fbf3b0011f749049987b0
Million Alemayehu <78154106+Million-Alemayehu@users.noreply.github.com>
2026-05-03 17:17:12 +0300

    testing with jest and eslint

--- meadowlark.js
+++ meadowlark.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 const fortune = require('./lib/fortune.js');
+const handlers = require('./lib/handlers.js')
 
 // 1️⃣ Static files FIRST
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,24 +18,21 @@ app.engine('handlebars', engine({
 app.set('view engine', 'handlebars');
 
 
-app.get('/', (req, res) => res.render('home')) 
+app.get('/', handlers.home) 
 
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortune.getFortune() })
-});
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res) =>{
-  res.status(404)
-  res.render('404')
-})
+app.use(handlers.notFound)
 
 //custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(handlers.serverError)
 
 
-app.listen(port, () => console.log(`Server running on port ${port}`+ `press Ctrl+C to terminate`));
+if(require.main===module){
+  app.listen(port,()=>{
+    console.log(`Express started on http://localhost:${pport}`+';press Ctrl-c to terminate.')
+  })
+}else{
+  module.exports = app
+}
